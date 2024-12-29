06c2ae881149a08fdd145c5f9146cd97849b8787
wawanpi <muhaguskurniawan@gmail.com>
2024-12-29 11:22:34 +0700

    Membuat data class Mahasiswa

--- app/src/main/java/com/example/pertemuan12/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan12/model/Mahasiswa.kt
@@ -4,15 +4,14 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Mahasiswa(
-    val nim : String,
-    val nama : String,
-    val alamat : String,
+data class Mahasiswa (
+    val nim: String,
+    val nama: String,
+    val alamat: String,
 
-    )
+    @SerialName("jenis_kelamin")
+    val jenisKelamin: String,
 
-@SerialName("jenis_kelamin")
-val jenisKelamin : String,
-val kelas: String,
-val angkatan: String,
+    val kelas: String,
+    val angkatan:String
 )
