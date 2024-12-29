f40d73311f2894c53da9b0e428c048a271a4a249
wawanpi <muhaguskurniawan@gmail.com>
2024-12-29 17:21:48 +0700

    Membuat Fungsi updateMhs

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/UpdateViewModel.kt
@@ -1,9 +1,40 @@
 package com.example.pertemuan12.ui.viewmodel
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
+// ViewModel untuk proses Update data Mahasiswa
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    // State untuk menyimpan inputan user
+    var uiState by mutableStateOf(UpdateUiState())
+        private set // Agar tidak dapat diubah langsung dari luar
 
+    // Update state input jika ada perubahan data
+    fun updateUpdateMhsState(updateUiEvent: UpdateUiEvent) {
+        uiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+
+    // Fungsi untuk update data mahasiswa
+    fun updateMhs() {
+        viewModelScope.launch { // Jalankan di background (async)
+            try {
+                mhs.updateMahasiswa(
+                    nim = uiState.updateUiEvent.nim, // Parameter NIM
+                    mahasiswa = uiState.updateUiEvent.toMhs() // Parameter Mahasiswa
+                )
+            } catch (e: Exception) {
+                e.printStackTrace() // Cetak error untuk debugging
+            }
+        }
+    }
+}
 
 // Data State untuk menyimpan state UI
 data class UpdateUiState(
